00afe765d817abee36868ff0e20047ee46b87bfe
Abdulakh Kurbanov <lankurbanov@gmail.com>
2024-08-07 23:24:46 +0200

    SoConfusing.
    Uploading the right file. I really should stop having duplicate folders.

--- HelloWorld/obj/Debug/net7.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net7.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc194392771f2c48827b2daf5ea0bccd87f8931e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85a1d544c856c566b04406694072c7ed804fdb89")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
